128e18f07c387027b7dbb6bd6f99a4dc15d08d22
Ekkyo <davyaubert@gmail.com>
2016-01-31 10:50:35 +0100

    FreebooksListActivity commit

--- app/src/main/java/com/aubert/freebooks/FreebooksListActivity.java
+++ app/src/main/java/com/aubert/freebooks/FreebooksListActivity.java
@@ -2,14 +2,14 @@ package com.aubert.freebooks;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class FreebooksListActivity extends Activity {
-    private final  String TAG = "FreebooksListActivity" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_freebooks_list);
     }
+
+
 }
